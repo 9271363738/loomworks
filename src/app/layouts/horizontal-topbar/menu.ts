08db892576935aa28d7b1665a2559527a5d577c1
shubham-zambre <shubhamzambre11@gmail.com>
2023-10-27 15:37:39 +0530

    add routing user

--- src/app/layouts/horizontal-topbar/menu.ts
+++ src/app/layouts/horizontal-topbar/menu.ts
@@ -1,6 +1,6 @@
 import { MenuItem } from './menu.model';
 
-export const MENU: MenuItem[] = [
+export const AdminMENU: MenuItem[] = [
   
   {
     id: 2,
@@ -27,4 +27,21 @@ export const MENU: MenuItem[] = [
     icon: 'bx bx-shopping-bag fs-22',
     link: '/admin/itemmaster',
   },
+]
+
+export const UserMENU: MenuItem[] = [
+  
+  {
+    id: 2,
+    label: 'Dashboard',
+    icon: 'ri-dashboard-2-line',
+    link: '/user/dashboard',
+  },
+  {
+    id: 8,
+    label: 'Orders',
+    icon: 'ri-apps-2-line',
+    link: '/user/order',
+  },
+
 ]
